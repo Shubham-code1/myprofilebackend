aa5dd0a707cd15edaf137dbbf20d407e29eb0def
shubham <shubham.email01@gmail.com>
2023-12-22 00:46:37 +0530

    changed connection database

--- connection.js
+++ connection.js
@@ -4,7 +4,7 @@ const Local_str = process.env.LOCAL_STRING;
 const Remote_str = process.env.REMOTE_STRING;
 
 
-module.exports = mongoose.connect(Remote_str)
+module.exports = mongoose.connect(Local_str,{ useNewUrlParser: true, useUnifiedTopology: true })
 .then(()=>{
     console.log("Database connection established");
 })
